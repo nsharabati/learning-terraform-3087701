1cfeff7d2b6d285df4a0724e3da817af9b62065d
nsharabati <95369348+nsharabati@users.noreply.github.com>
2023-01-17 15:51:05 +0100

    ..

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ module "alb" {
       target_type      = "instance"
       targets = {
         my_target = {
-          target_id = aws_instance.blog.id
+          target_id = aws_instance.blogs.id
           port = 80
         }
       }
