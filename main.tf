05845f4b974b77fc8d17871187f327af2f321119
nsharabati <95369348+nsharabati@users.noreply.github.com>
2023-01-17 15:44:04 +0100

    load balancer

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ module "vpc" {
 resource "aws_instance" "blogs" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
- # instance_type = "t3.nano"
+
   
   vpc_security_group_ids = [module.blog_sg.security_group_id]
   
@@ -54,6 +54,51 @@ resource "aws_instance" "blogs" {
 # resource "aws_security_group" "blog"{
 #  name        = "blog"
 #  description = "Allow http and https in. Allow everything out"
+
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 8.0"
+
+  name = "blog-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = module.blog_sg.security_group_id
+
+ 
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
 
 module "blog_sg" {
 
