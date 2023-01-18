0be13acca2000a913876107cfa15e3a2b9994819
nsharabati <95369348+nsharabati@users.noreply.github.com>
2023-01-18 15:05:49 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ module "autoscaling" {
 
   name     = "${var.environment.name}-blogs"
   min_size = var.asg_min_size
-  max_size = var.asg_max.size
+  max_size = var.asg_max_size
 
   vpc_zone_identifier    = module.vpc.public_subnets
   target_group_arns      = module.blog_alb.target_group_arns
